8611936ce0f696e15ed60647ff2c59ede55b1000
richardborgesdev <richardborgesdev@gmail.com>
2025-01-30 09:50:31 -0300

    chore: handle timer

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {}
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button>Start challenge</button>
-      <p className="active">time is running... / timer inactive</p>
+      <button onClick={handleStart}>
+        {timerStarted ? "Stop" : "Start"} challenge
+      </button>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "time is running..." : "timer inactive"}
+      </p>
     </section>
   );
 }
